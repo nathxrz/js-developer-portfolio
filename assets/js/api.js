c81657eeafe97f79a971b0706b85790e738a02ca
Nathalia Machado <nathyrezendemachado@gmail.com>
2025-08-26 21:41:33 -0300

    add info profile

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 async function fetchProfileData() {
   const url =
-    "https://raw.githubusercontent.com/nathxrz/js-developer-portfolio/refs/heads/projeto-base/data/profile.json";
+    "https://raw.githubusercontent.com/nathxrz/js-developer-portfolio/refs/heads/main/data/profile.json";
   const fetching = await fetch(url);
   return await fetching.json();
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,42 @@
+function updateProfile(profileData) {
+  const profilePhoto = document.getElementById("profile.photo");
+  profilePhoto.src = profileData.photo;
+  profilePhoto.alt = profileData.name;
+
+  const profileName = document.getElementById("profile.name");
+  profileName.innerText = profileData.name;
+
+  const profileJob = document.getElementById("profile.job");
+  profileJob.innerText = profileData.job;
+  const profileLocation = document.getElementById("profile.location");
+  profileLocation.innerText = profileData.location;
+
+  const profilePhone = document.getElementById("profile.phone");
+  profilePhone.innerText = profileData.phone;
+
+  const profileEmail = document.getElementById("profile.email");
+  profileEmail.innerText = profileData.email;
+}
+
+function updateSkills(profileData) {
+  const skillsList = document.getElementById("skills.list");
+
+  const items = profileData.skills.hardSkills.map((skill) => {
+    return `
+    <li>
+      <img
+        src="${skill.logo}"
+        alt="${skill.name}"
+        title="${skill.name}"
+      />
+    </li>`;
+  });
+  skillsList.innerHTML = items.join("");
+}
+
 function updateProfileInfo(profileData) {
-  const profilePhoto = document.querySelector(".profile-photo");
-  profilePhoto.setAttribute("src", profileData.photo);
+  updateProfile(profileData);
+  updateSkills(profileData);
 }
 
 (async () => {
